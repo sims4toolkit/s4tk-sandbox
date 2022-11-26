3c4e12479e7661f77821831c5f89e5ae666a9114
Frank Kulak <fs.kulak@gmail.com>
2022-11-26 16:21:37 -0500

    Initial commit.

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Navbar from "./components/Navbar.svelte";
+  import ModeSwitcher from "./components/ModeSwitcher.svelte";
+  import Tailwindcss from "./Tailwindcss.svelte";
+</script>
+
+<Tailwindcss />
+<ModeSwitcher />
+<Navbar />
+<main class="p-4 mx-auto text-center max-w-xl">
+  <p>Test</p>
+</main>
+
+<style lang="scss">
+  // intentionally blank
+</style>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  // TODO
+</script>
+
+<nav
+  class="flex flex-nowrap items-center px-2 py-4 dark:bg-zinc-800 bg-slate-400"
+>
+  <img src="./favicon.png" alt="Icon" class="h-8 mr-2" />
+  <h4>Name</h4>
+
+  <button class="button bg-white p-1">hi</button>
+</nav>
+
+<style lang="scss">
+  // TODO
+</style>
