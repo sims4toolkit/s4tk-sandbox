51d64a88c66d8ed531ca657fb5e88b4af7fda6e2
Frank Kulak <fs.kulak@gmail.com>
2022-11-26 17:13:40 -0500

    More boilerplate.

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
+  import Router from "svelte-spa-router";
+  import Footer from "./components/Footer.svelte";
   import Navbar from "./components/Navbar.svelte";
+  import HomePage from "./pages/HomePage.svelte";
+  import NotFoundPage from "./pages/NotFoundPage.svelte";
+
+  const routes = {
+    "/": HomePage,
+    "*": NotFoundPage,
+  };
 </script>
 
 <Navbar />
-<main class="p-4 mx-auto text-center max-w-xl">
-  <p>Test</p>
+<main class="min-h-screen flex flex-col">
+  <Router {routes} restoreScrollState={true} />
 </main>
-
-<style lang="scss">
-  // intentionally blank
-</style>
+<Footer />
