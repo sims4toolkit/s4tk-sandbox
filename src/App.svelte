a88e33a3d6bcb7d927278869c42d83cf70fd458c
Frank Kulak <fs.kulak@gmail.com>
2022-11-27 10:25:53 -0500

    Rename HomePage > SandboxPage.

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import Router from "svelte-spa-router";
   import Navbar from "src/components/Navbar.svelte";
-  import HomePage from "src/pages/HomePage.svelte";
+  import SandboxPage from "src/pages/SandboxPage.svelte";
   import NotFoundPage from "src/pages/NotFoundPage.svelte";
   import HelpPage from "src/pages/HelpPage.svelte";
 
   const routes = {
-    "/": HomePage,
+    "/": SandboxPage,
     "/help": HelpPage,
     "*": NotFoundPage,
   };
