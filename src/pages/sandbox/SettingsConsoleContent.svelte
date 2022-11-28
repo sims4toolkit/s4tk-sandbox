ef6dbe536e9911b0f4c53f82d04ea049c80acb4d
Frank Kulak <fs.kulak@gmail.com>
2022-11-28 11:40:06 -0500

    Update SettingsConsoleContent.svelte

--- src/pages/sandbox/SettingsConsoleContent.svelte
+++ src/pages/sandbox/SettingsConsoleContent.svelte
@@ -2,10 +2,10 @@
   import DatabaseService from "src/lib/database";
   import Settings from "src/lib/settings";
 
-  async function clearCache() {
+  async function clearApiCache() {
     if (
       confirm(
-        `This will delete all of the S4TK API versions that have been cached in your browser, and the page with reload and re-fetch the API from the server. Are you sure you want to continue?`
+        `This will delete all of the cached S4TK API versions in your browser, and your current version will be re-fetched from the server. Are you sure you want to continue?`
       )
     ) {
       await DatabaseService.clear("api");
@@ -14,23 +14,13 @@
     }
   }
 
-  async function deleteMedia() {
+  async function deleteUserData() {
     if (
       confirm(
-        `This will delete all of your media files, and there is no way to recover them. Are you sure you want to continue?`
+        `This will permanently delete all of your scripts and media files, and there is no way to recover them. Are you sure you want to continue?`
       )
     ) {
       await DatabaseService.clear("media");
-      location.reload();
-    }
-  }
-
-  async function deleteScripts() {
-    if (
-      confirm(
-        `This will delete all of your scripts, and there is no way to recover them. Are you sure you want to continue?`
-      )
-    ) {
       await DatabaseService.clear("script");
       Settings.hasSeenExampleScript = false;
       location.reload();
@@ -41,14 +31,10 @@
 <div class="flex gap-4">
   <button
     class="text-red-600 dark:text-red-500 border border-solid border-red-600 dark:border-red-500 hover:bg-red-600 hover:text-white dark:hover:bg-red-500 dark:hover:text-gray-900 px-2 py-1 rounded"
-    on:click={deleteScripts}>Delete Scripts</button
+    on:click={clearApiCache}>Clear API Cache</button
   >
   <button
     class="text-red-600 dark:text-red-500 border border-solid border-red-600 dark:border-red-500 hover:bg-red-600 hover:text-white dark:hover:bg-red-500 dark:hover:text-gray-900 px-2 py-1 rounded"
-    on:click={deleteMedia}>Delete Media</button
-  >
-  <button
-    class="text-red-600 dark:text-red-500 border border-solid border-red-600 dark:border-red-500 hover:bg-red-600 hover:text-white dark:hover:bg-red-500 dark:hover:text-gray-900 px-2 py-1 rounded"
-    on:click={clearCache}>Clear Cache</button
+    on:click={deleteUserData}>Delete User Data</button
   >
 </div>
