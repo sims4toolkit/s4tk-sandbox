595950d98fe95729cc568fa74f68230ffb46ff30
Frank Kulak <fs.kulak@gmail.com>
2022-11-27 21:23:59 -0500

    Update settings.

--- src/pages/sandbox/ConsolePanel.svelte
+++ src/pages/sandbox/ConsolePanel.svelte
@@ -8,14 +8,37 @@
 
   const outputPlaceholder = `Errors and calls to <code class="text-primary">Sandbox.output(...args: string[])</code> will be written here. Asynchronous output might not get written.`;
 
-  function clearCache() {
+  async function clearCache() {
     if (
       confirm(
-        `This will delete all of the S4TK API versions that have been cached in your browser, meaning they will have to be fetched from the server the next time the page is reloaded. Are you sure you want to continue?`
+        `This will delete all of the S4TK API versions that have been cached in your browser, and the page with reload and re-fetch the API from the server. Are you sure you want to continue?`
       )
     ) {
-      DatabaseService.clear("api");
-      DatabaseService.clear("api_specs");
+      await DatabaseService.clear("api");
+      await DatabaseService.clear("api_specs");
+      location.reload();
+    }
+  }
+
+  async function deleteMedia() {
+    if (
+      confirm(
+        `This will delete all of your media files, and there is no way to recover them. Are you sure you want to continue?`
+      )
+    ) {
+      await DatabaseService.clear("media");
+      location.reload();
+    }
+  }
+
+  async function deleteScripts() {
+    if (
+      confirm(
+        `This will delete all of your scripts, and there is no way to recover them. Are you sure you want to continue?`
+      )
+    ) {
+      await DatabaseService.clear("script");
+      location.reload();
     }
   }
 </script>
@@ -72,10 +95,20 @@
     {:else if currentTab === "Help"}
       <HelpConsoleContent />
     {:else}
-      <button
-        class="text-red-600 dark:text-red-500 border border-solid border-red-600 dark:border-red-500 hover:bg-red-600 hover:text-white dark:hover:bg-red-500 dark:hover:text-gray-900 px-2 py-1 rounded"
-        on:click={clearCache}>Clear Cache</button
-      >
+      <div class="flex gap-4">
+        <button
+          class="text-red-600 dark:text-red-500 border border-solid border-red-600 dark:border-red-500 hover:bg-red-600 hover:text-white dark:hover:bg-red-500 dark:hover:text-gray-900 px-2 py-1 rounded"
+          on:click={deleteScripts}>Delete Scripts</button
+        >
+        <button
+          class="text-red-600 dark:text-red-500 border border-solid border-red-600 dark:border-red-500 hover:bg-red-600 hover:text-white dark:hover:bg-red-500 dark:hover:text-gray-900 px-2 py-1 rounded"
+          on:click={deleteMedia}>Delete Media</button
+        >
+        <button
+          class="text-red-600 dark:text-red-500 border border-solid border-red-600 dark:border-red-500 hover:bg-red-600 hover:text-white dark:hover:bg-red-500 dark:hover:text-gray-900 px-2 py-1 rounded"
+          on:click={clearCache}>Clear Cache</button
+        >
+      </div>
     {/if}
   </div>
 </div>
