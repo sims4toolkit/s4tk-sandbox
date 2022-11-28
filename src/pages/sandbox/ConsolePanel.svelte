199016ae3f3f5ef6257fe2562f613e6571621a5c
Frank Kulak <fs.kulak@gmail.com>
2022-11-27 21:25:40 -0500

    Move settings content to own file.

--- src/pages/sandbox/ConsolePanel.svelte
+++ src/pages/sandbox/ConsolePanel.svelte
@@ -1,46 +1,11 @@
 <script lang="ts">
-  import DatabaseService from "src/lib/database";
-  import { S4TK_API_STATE } from "src/lib/s4tk-api";
-  import HelpConsoleContent from "./HelpConsoleContent.svelte";
+  import HelpConsoleContent from "src/pages/sandbox/HelpConsoleContent.svelte";
+  import SettingsConsoleContent from "src/pages/sandbox/SettingsConsoleContent.svelte";
 
   export let output: string;
   export let currentTab: "Output" | "Help" | "Settings" = "Output";
 
   const outputPlaceholder = `Errors and calls to <code class="text-primary">Sandbox.output(...args: string[])</code> will be written here. Asynchronous output might not get written.`;
-
-  async function clearCache() {
-    if (
-      confirm(
-        `This will delete all of the S4TK API versions that have been cached in your browser, and the page with reload and re-fetch the API from the server. Are you sure you want to continue?`
-      )
-    ) {
-      await DatabaseService.clear("api");
-      await DatabaseService.clear("api_specs");
-      location.reload();
-    }
-  }
-
-  async function deleteMedia() {
-    if (
-      confirm(
-        `This will delete all of your media files, and there is no way to recover them. Are you sure you want to continue?`
-      )
-    ) {
-      await DatabaseService.clear("media");
-      location.reload();
-    }
-  }
-
-  async function deleteScripts() {
-    if (
-      confirm(
-        `This will delete all of your scripts, and there is no way to recover them. Are you sure you want to continue?`
-      )
-    ) {
-      await DatabaseService.clear("script");
-      location.reload();
-    }
-  }
 </script>
 
 <div class="flex flex-col h-full overflow-hidden">
@@ -95,20 +60,7 @@
     {:else if currentTab === "Help"}
       <HelpConsoleContent />
     {:else}
-      <div class="flex gap-4">
-        <button
-          class="text-red-600 dark:text-red-500 border border-solid border-red-600 dark:border-red-500 hover:bg-red-600 hover:text-white dark:hover:bg-red-500 dark:hover:text-gray-900 px-2 py-1 rounded"
-          on:click={deleteScripts}>Delete Scripts</button
-        >
-        <button
-          class="text-red-600 dark:text-red-500 border border-solid border-red-600 dark:border-red-500 hover:bg-red-600 hover:text-white dark:hover:bg-red-500 dark:hover:text-gray-900 px-2 py-1 rounded"
-          on:click={deleteMedia}>Delete Media</button
-        >
-        <button
-          class="text-red-600 dark:text-red-500 border border-solid border-red-600 dark:border-red-500 hover:bg-red-600 hover:text-white dark:hover:bg-red-500 dark:hover:text-gray-900 px-2 py-1 rounded"
-          on:click={clearCache}>Clear Cache</button
-        >
-      </div>
+      <SettingsConsoleContent />
     {/if}
   </div>
 </div>
