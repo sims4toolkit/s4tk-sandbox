868fe59cb34aeb9cf9639b04acdfc8e32030526f
Frank Kulak <fs.kulak@gmail.com>
2022-11-28 12:05:24 -0500

    Fix z index issue.

--- src/components/elements/FloatingActionButtonGroup.svelte
+++ src/components/elements/FloatingActionButtonGroup.svelte
@@ -16,7 +16,7 @@
   }
 </script>
 
-<div class="fixed right-6 bottom-6 z-0">
+<div class="fixed right-6 bottom-6 z-10">
   {#if showTitle}
     <FloatingActionButtonTitle text={titleText} color={titleColor} {disabled} />
   {/if}
