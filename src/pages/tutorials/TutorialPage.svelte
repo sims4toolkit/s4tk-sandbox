c33d8c1b64d79bece713e4c55f190174338bf6a2
Frank Kulak <fs.kulak@gmail.com>
2022-11-29 21:04:21 -0500

    Update TutorialPage.svelte

--- src/pages/tutorials/TutorialPage.svelte
+++ src/pages/tutorials/TutorialPage.svelte
@@ -7,16 +7,22 @@
   import { updateEditorContent } from "src/lib/editor";
   import VerticalSplitView from "src/components/layout/VerticalSplitView.svelte";
   import ApiVersionSwitcher from "src/components/editor/ApiVersionSwitcher.svelte";
-  import { fetchTutorial, Tutorial } from "src/lib/tutorials";
+  import {
+    fetchTutorial,
+    fetchTutorialsIndex,
+    Tutorial,
+  } from "src/lib/tutorials";
 
   export let params: { name: string };
 
   let sandboxEditor: SandboxEditor;
   let buttonData: FloatingActionButtonData[] = [];
   let currentScriptContent: string;
-  let currentScriptName = params.name; // FIXME: what if none?
   let editor: EditorView;
+
   let fetchedTutorial: Tutorial;
+  let tutorialName = params.name;
+  let tutorialDescription = "";
 
   onMount(() => {
     buttonData = [
@@ -31,12 +37,7 @@
         title: "Reset",
         icon: "refresh",
         onClick() {
-          if (fetchedTutorial) {
-            updateEditorContent(editor, fetchedTutorial.script);
-          } else {
-            // TODO: error handling
-            console.error("Script was never fetched");
-          }
+          updateEditorContent(editor, fetchedTutorial.script);
         },
       },
       {
@@ -46,6 +47,19 @@
         onClick: sandboxEditor.runEditorScript,
       },
     ];
+
+    fetchTutorialsIndex()
+      .then((index) => {
+        const tutorialData = index.tutorials[params.name];
+        if (tutorialData) {
+          tutorialName = tutorialData.name;
+          tutorialDescription = tutorialData.description;
+          // TODO: also make sure required API version is used
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
 
     fetchTutorial(params.name)
       .then((tutorial) => {
@@ -60,21 +74,37 @@
 </script>
 
 <svelte:head>
-  <title>Tutorial | {params.name}</title>
+  <title>Tutorial | {tutorialName}</title>
 </svelte:head>
 
 <div class="fixed top-10 left-0 right-0 bottom-0 dark:bg-gray-900">
   <VerticalSplitView leftPanelName="Tutorial Manager">
     <div slot="left" class="absolute left-0 right-0 top-0 bottom-0">
       <ApiVersionSwitcher fixedVersion="0.1.0" />
-      <p>Test</p>
+      {#if Boolean(fetchedTutorial)}
+        <div class="p-2">
+          <div class="overflow-hidden whitespace-normal break-words">
+            <h4 class="text-primary font-bold mb-1">
+              {tutorialName}
+            </h4>
+            <p class="text-subtle text-sm mb-4">
+              {tutorialDescription}
+            </p>
+          </div>
+
+          <div class="whitespace-pre-wrap">
+            <!-- FIXME: this is markdown -->
+            {fetchedTutorial.guide}
+          </div>
+        </div>
+      {/if}
     </div>
     <SandboxEditor
       slot="right"
       bind:this={sandboxEditor}
       bind:buttonData
       bind:currentScriptContent
-      bind:currentScriptName
+      bind:currentScriptName={tutorialName}
       bind:editor
       useFileSystem={false}
       isTutorial={true}
