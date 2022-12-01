001038da98bdf31aa325baf4dea4a4e3dcac6b0d
Frank Kulak <fs.kulak@gmail.com>
2022-12-01 16:54:55 -0500

    Update TutorialTag.svelte

--- src/pages/tutorials/TutorialTag.svelte
+++ src/pages/tutorials/TutorialTag.svelte
@@ -6,20 +6,24 @@
   <p class="text-xs px-1 bg-sky-300 text-sky-900 rounded max-w-min">
     {tag}
   </p>
-{:else if tag === "Utilities"}
+{:else if tag === "Beginner"}
   <p class="text-xs px-1 bg-emerald-300 text-emerald-900 rounded max-w-min">
     {tag}
   </p>
 {:else if tag === "Advanced"}
-  <p class="text-xs px-1 bg-pink-300 text-pink-900 rounded max-w-min">
+  <p class="text-xs px-1 bg-rose-300 text-rose-900 rounded max-w-min">
     {tag}
   </p>
 {:else if tag === "Models"}
   <p class="text-xs px-1 bg-purple-300 text-purple-900 rounded max-w-min">
     {tag}
   </p>
+{:else if tag === "Utilities"}
+  <p class="text-xs px-1 bg-amber-300 text-amber-900 rounded max-w-min">
+    {tag}
+  </p>
 {:else}
-  <p class="text-xs px-1 bg-orange-300 text-orange-900 rounded max-w-min">
+  <p class="text-xs px-1 bg-zinc-300 text-zinc-900 rounded max-w-min">
     {tag}
   </p>
 {/if}
